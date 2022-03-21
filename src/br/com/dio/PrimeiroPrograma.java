bd22633f3f4b07f331c938625feb744c4e4c8a9c
DanielSilvaMonteiro <monteiro.dan@outlook.com>
2022-03-20 22:59:55 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+//ctrl + Y apaga linha
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+/*
+        ctrl + shift + / comenta o bloco
+        int a = 5, b = 3;
+        System.out.println("hello world! " + (a+b));*/
+
+
+    }
+}
